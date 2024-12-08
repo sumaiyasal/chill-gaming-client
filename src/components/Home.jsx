6093c26cf804d4b30dd02ba7b21314c257535eea
sumaiya salim <sumaiyasalim062@gmail.com>
2024-12-08 17:26:05 +0600

    added highest rated section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,12 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 const Home = () => {
+  const[highestreview,setHighestreview]=useState([]);
+  fetch('http://localhost:5000/highestreviews')
+  .then(res=>res.json())
+  .then(data=>setHighestreview(data))
+
     return (
         <div className="container mx-auto">
             <section>
@@ -40,7 +48,45 @@ const Home = () => {
   </div>
 </div>
             </section>
+            <section>
+              
+              <h1 className="text-center">Highest Rated Game</h1>
+              <div className="grid grid-cols-3 gap-8">
+            {highestreview.map(hreview=>
+             <div>
+             <div className="card bg-base-100 w-96 shadow-xl">
+    <figure>
+      <img
+        src={hreview.gameimage} className="w-[300px] h-[300px] pt-10"
+        alt="Shoes" />
+    </figure>
+    <div className="card-body pl-16">
+      <h2 className="card-title">{hreview.gamename}</h2>
+      <p className="text-xl">{hreview.rating} <div className="rating">
+    <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+    <input
+      type="radio"
+      name="rating-2"
+      className="mask mask-star-2 bg-orange-400"
+      defaultChecked />
+    <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+    <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+    <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+  </div></p>
+      <p>Publish in {hreview.publishingyear}</p>
+      <div className="card-actions justify-end">
+          <Link to={`/allreviews/${hreview._id}`}><button className="btn btn-primary">Explore Details</button></Link>
+        
+      </div>
+    </div>
+  </div> 
+          </div>
+
+            )}
+            </div>
+            </section>
         </div>
+
     );
 };
 
