c6cd8960c3b1c650745f50a9783a04f9370b3df2
sumaiya salim <sumaiyasalim062@gmail.com>
2024-12-09 11:02:07 +0600

    added error page

--- src/components/AddReview.jsx
+++ src/components/AddReview.jsx
@@ -22,8 +22,7 @@ const AddReview = () => {
          const newreview={gameimage,gamename,review,rating,publishingyear,genres,email,displayName}
 
         
-        fetch('chill-gaming-server-48g4fkiq0-sumaiya-s-projects-efb56ee6.vercel.app
-/reviews', {
+        fetch('chill-gaming-server-48g4fkiq0-sumaiya-s-projects-efb56ee6.vercel.app/reviews', {
             method: 'POST',
             headers: {
                 'content-type': 'application/json'
